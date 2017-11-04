5e44ed9c6535adbce2b8dfe864447dc167743c44
Bùi Lê Duy Nhất <kimegitee@gmail.com>
2017-11-05 04:55:17 +0700

    Add staircase problem

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-import hey
+import hey, staircase
 
 class HeyTest(unittest.TestCase):
 
@@ -8,8 +8,16 @@ class HeyTest(unittest.TestCase):
 
 	def test_getnext(self):
 		self.assertEqual(hey.getnext('210111', 3), '122221')
-		self.assertEqual(hey.getnext('122221', 3), '102212')	
+		self.assertEqual(hey.getnext('122221', 3), '102212')
 
 	def test_answer(self):
 		self.assertEqual(hey.answer('1211', 10), 1)
 		self.assertEqual(hey.answer('210022', 3), 3)
+
+class StairTest(unittest.TestCase):
+
+	def test_answer(self):
+		self.assertEqual(staircase.answer(3), 1)
+		self.assertEqual(staircase.answer(4), 1)
+		self.assertEqual(staircase.answer(5), 2)
+		self.assertEqual(staircase.answer(200), 487067745)
